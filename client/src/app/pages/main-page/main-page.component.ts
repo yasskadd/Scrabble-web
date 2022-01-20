7b12937b9d419ba59c930f77962e3c70a43d21c5
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2022-01-20 17:08:06 -0500

    Added Dialog

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { DialogBoxComponent } from '@app/components/dialog-box/dialog-box.component';
 
 @Component({
     selector: 'app-main-page',
@@ -10,4 +11,10 @@ export class MainPageComponent {
     readonly title: string = 'LOG2990';
 
     constructor(public dialog: MatDialog) {}
+
+    openDialog(): void {
+        const dialogReference = this.dialog.open(DialogBoxComponent, {});
+
+        dialogReference.afterClosed().subscribe();
+    }
 }
