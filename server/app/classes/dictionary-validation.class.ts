2f91495ea95d096b4989a5f966abf50f43f413cb
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2022-01-26 14:34:26 -0500

    set TODOs in dictionary validation file

--- server/app/classes/dictionary-validation.class.ts
+++ server/app/classes/dictionary-validation.class.ts
@@ -1,20 +1,29 @@
+import dictionnaryData from '../app/assets/dictionary.json';
 import { Word } from './word';
 
+// TODO : import dictionary
 let dictionary = new Set<string>();
 
-dictionary.forEach((word: string) => {
-    OurDictionary.add(word);
+dictionnaryData.words.forEach((word: string) => {
+    dictionary.add(word);
 });
 
+// TODO : detect words (once placement validation is done)
+
+// TODO : validate detected words
 function validateWords(enteredWords: Array<Word>) {
+    // vérifier que les mots sont valides
     enteredWords.forEach((enteredWord) => {
-        if (dictionary.has(enteredWord.stringFormat)) {
-            enteredWord.isValid = true;
-        } else {
+        if (!dictionary.has(enteredWord.stringFormat)) {
             enteredWord.isValid = false;
-            // break (send message to take placed letters off board)
+            console.log(enteredWord + 'is not valid');
+        } else {
+            enteredWord.isValid = true;
+            console.log(enteredWord + 'is not valid');
         }
     });
-    // envoyer message pour placer lettres sur chevalet si on a
-    // pas break, terminer le tour, ...
+    // TODO : if at least one word is not valid.
+    // (in client) flash invalid words red +
+    // envoyer requete pour enlever lettres du grid.
+    // TODO : else, envoyer requete pour compter les points.
 }
