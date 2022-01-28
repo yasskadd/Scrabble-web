e5760f893dd70b2d37d07cbd68fb1f3a4e8873ec
LaurentNg <laurentnguyen82@gmail.com>
2022-01-28 00:56:59 -0500

    Letter-reserve logic mostly done, few eslint error

--- server/app/services/letter-reserve.service.ts
+++ server/app/services/letter-reserve.service.ts
@@ -1,52 +1,94 @@
 // import { HttpClient } from '@angular/common/http';
 import letterJSON from '../app/assets/letter-reserve.json';
 
-interface LetterScheme {
+// Temporary place
+interface Letter {
     letter: string;
     quantity: number;
     weight: number;
 }
 
 export class LetterReserveService {
-    lettersReserve: [];
-    playerRack: [];
+    lettersReserve: Letter[];
+    playerRack: Letter[] = [];
     constructor() {
         this.lettersReserve = this.getLetterReserve();
     }
 
-    getLetterReserve(): [] {
+    /**
+     * Get the default letter reserve.
+     *
+     * @return Letter[] : Return a list of letters.
+     */
+    getLetterReserve(): Letter[] {
         return JSON.parse(letterJSON).letters;
+        // return letterJSON.letters;
     }
 
-    updateReserve(letter: LetterScheme): void {
-        // modify json file with new quantities
-        // this.lettersReserve['letters'][0].quantity -= 1;
-        // this.lettersReserve.find( obj => obj.letter === letter;  ).quantity--;
-        letter.quantity -= 1;
+    /**
+     * Update the letter reserve
+     *
+     * @param letter : The letter that has to be updated in the reserve.
+     */
+    updateReserve(letter: Letter): void {
+        letter.quantity--;
+        this.lettersReserve = this.lettersReserve.filter((letter) => letter.quantity !== 0);
     }
 
-    addLetter(): LetterScheme {
-        const max = 27; // lettersReserve.length
-        const random = Math.floor(Math.random() * max);
+    /**
+     * The reserve gives a random letter from the letter reserve to a player.
+     */
+    addLetter(): void {
+        const nLetters = this.lettersReserve.length;
+        const random = Math.floor(Math.random() * nLetters);
         const letter = this.lettersReserve[random];
         this.updateReserve(letter);
         this.playerRack.push(letter);
-        return letter;
     }
 
-    generateLetters(): void {
-        // start of the game
-        const initializeLetters = 7;
-        for (let i = 0; i < initializeLetters; i++) {
-            const letter: LetterScheme = this.addLetter();
-            this.updateReserve(letter);
+    /**
+     * Exchange letters
+     *
+     * @param letters : The letters that the player wants to exchange.
+     */
+    exchangeLetter(letters: Letter[]) {
+        // Remove the letters from the rack of the player
+        this.playerRack = this.playerRack.filter((letter) => !letters.includes(letter));
+
+        // Exchange X quantity of letters
+        this.generateLetters(letters.length);
+
+        // Put back the letters exchanged into the reserve
+        letters.forEach((letter) => {
+            if (letter.quantity === 0 && !this.lettersReserve.includes(letter)) {
+                this.lettersReserve.push(letter);
+            }
+        });
+
+        for (let i = 0; i < letters.length; i++) {
+            for (let j = 0; j < this.lettersReserve.length; j++) {
+                if (letters[i].letter === this.lettersReserve[j].letter) {
+                    this.lettersReserve[j].quantity++;
+                }
+            }
         }
     }
 
-    ngOnInit(): void {
-        // this.lettersReserve = this.getLetterReserve().letters;
-        this.generateLetters();
+    /**
+     * The letter reserve gives X quantity of random letter to a player.
+     *
+     * @param quantity : The number of letter to be given from the letter reserve to a player.
+     */
+    generateLetters(quantity: number): void {
+        for (let i = 0; i < quantity; i++) {
+            this.addLetter();
+        }
     }
+
+    // ngOnInit(): void {
+    //     // this.lettersReserve = this.getLetterReserve().letters;
+    //     this.generateLetters();
+    // }
 }
 // GENERATE RANDOM LETTER
 const letters = [
