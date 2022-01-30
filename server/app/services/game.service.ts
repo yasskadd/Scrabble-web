6f7a5a05824a150b73eb11b303742dd618c33a91
LaurentNg <laurentnguyen82@gmail.com>
2022-01-29 21:31:50 -0500

    Updating game logic by adding timer

--- server/app/services/game.service.ts
+++ server/app/services/game.service.ts
@@ -17,57 +17,91 @@ export class GameService {
     player1: Player;
     player2: Player;
     letterReserve: LetterReserveService;
-    time: number;
     turn: TurnService;
 
-    constructor(player1: Player, player2: Player) {
-        this.start(player1, player2);
+    constructor(player1: Player, player2: Player, time: number) {
+        this.start(player1, player2, time);
     }
 
-    start(player1: Player, player2: Player) {
+    /**
+     * Start a game.
+     *
+     * @param player1 : The first player in the game.
+     * @param player2 : The second player in the game.
+     * @param time    : The maximum time allowed per player's turn.
+     */
+    start(player1: Player, player2: Player, time: number): void {
         this.player1 = player1;
         this.player2 = player2;
-        this.turn = new TurnService();
+        this.turn = new TurnService(time);
         this.letterReserve = new LetterReserveService();
 
         this.letterReserve.generateLetters(MAX_QUANTITY, this.player1.rack);
         this.letterReserve.generateLetters(MAX_QUANTITY, this.player2.rack);
-        this.turn.determinePlayerTurn(this.player1, this.player2);
+        this.turn.determinePlayer(this.player1, this.player2);
+        this.turn.start();
     }
 
-    end() {
-        this.turn.endTurn(null);
+    /**
+     * End a game.
+     *
+     */
+    end(): void {
+        this.turn.end(undefined);
         // console.log("Game has ended");
     }
 
-    skip(player1: Player, player2: Player) {
-        if (this.turn.validatingTurn(player1)) {
-            this.turn.endTurn(player2);
+    /**
+     * Skip a turn.
+     *
+     * @param player1 : The active player who will skip.
+     * @param player2 : The next active player.
+     */
+    skip(player1: Player, player2: Player): void {
+        if (this.turn.validating(player1)) {
+            this.turn.end(player2);
         } else {
             // console.log('Not your turn');
         }
     }
 
-    play(player1: Player, player2: Player) {
-        if (this.turn.validatingTurn(player1)) {
+    /**
+     * Play the turn by placing letters.
+     *
+     * @param player1 : The active player who will play.
+     * @param player2 : The next active player.
+     */
+    play(player1: Player, player2: Player): void {
+        if (this.turn.validating(player1)) {
             // TODO Add place letter logic
             // console.log('play');
-            this.turn.endTurn(player2);
+            this.turn.end(player2);
         } else {
             // console.log('Not your turn');
         }
     }
 
-    exchange(player1: Player, player2: Player, letters: Letter[]) {
-        if (this.turn.validatingTurn(player1)) {
+    /**
+     * Exchange letters.
+     *
+     * @param player1 : The active player who will exchange.
+     * @param player2 : The next active player.
+     * @param letters : The letters to exchange.
+     */
+    exchange(player1: Player, player2: Player, letters: Letter[]): void {
+        if (this.turn.validating(player1)) {
             player1.rack = this.letterReserve.exchangeLetter(letters, player1.rack);
-            this.turn.endTurn(player2);
+            this.turn.end(player2);
         } else {
             // console.log('Not your turn');
         }
     }
 
-    abandon() {
+    /**
+     * Abandon a game, which will resume in a lost for the player who abandoned.
+     *
+     */
+    abandon(): void {
         this.end();
     }
 }
