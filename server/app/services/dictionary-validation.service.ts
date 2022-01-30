b9290c3e194bcc104a865c327aa654ae415b0a98
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2022-01-30 01:37:31 -0500

    restructed architecture : word-score service was replaced by a method in class Word, gameboard-box was replaced by adding appropriate attribute to class Coordinate, you can now access information linked to a gameboard square by tapping into the coordinate object

--- server/app/services/dictionary-validation.service.ts
+++ server/app/services/dictionary-validation.service.ts
@@ -1,30 +1,36 @@
+import { GameBoard } from 'app/classes/gameboard.class';
 import { Service } from 'typedi';
 import * as jsonDictionnary from '../../assets/dictionary.json';
-import { Word } from '../classes/word';
+import { Word } from '../classes/word.class';
 
 @Service()
 export class DictionaryValidationService {
     dictionary: Set<string>;
+    gameboard: GameBoard; // not sure if this is good
 
-    /**
-     * Adds all dictionnary words from a json file into a Set object.
-     * @dictionaryLanguage
-     *
-     * N.B. : Using a Set later allows to check if a word exists
-     * in the dictionnary with a time-complexity of O(n)
-     */
     constructor() {
         (<any>jsonDictionnary).words.forEach((word: string) => {
             this.dictionary.add(word);
         });
     }
 
-    /**
-     * Check if newly placed letters on the gameboard form valid words
-     * (i.e. words that are present in the dictionary)
-     * @enteredWords represents newly formed words on the gameboard
-     */
     validateWords(enteredWords: Array<Word>): void {
+        this.checkWordInDictionary(enteredWords);
+        const invalidWords = this.isolateInvalidWords(enteredWords);
+
+        if (!invalidWords) {
+            // TODO : calculatePoints() + endTurn()
+            let newPoints: number = 0;
+            enteredWords.forEach((word: Word) => {
+                newPoints += word.calculatePoints(word, this.gameboard);
+                // end turn
+            });
+        } else {
+            // TODO : flash invalideWords red and removeLetters();
+        }
+    }
+
+    checkWordInDictionary(enteredWords: Array<Word>): void {
         enteredWords.forEach((enteredWord) => {
             if (!this.dictionary.has(enteredWord.stringFormat)) {
                 enteredWord.isValid = false;
@@ -34,15 +40,11 @@ export class DictionaryValidationService {
                 console.log(enteredWord + 'is not valid');
             }
         });
+    }
 
-        const invalidWords = enteredWords.filter((word) => {
+    isolateInvalidWords(enteredWords: Array<Word>) {
+        return enteredWords.filter((word) => {
             word.isValid = false;
         });
-
-        if (!invalidWords) {
-            // TODO : calculatePoints() + endTurn()
-        } else {
-            // TODO : flash invalideWords red and removeLetters();
-        }
     }
 }
