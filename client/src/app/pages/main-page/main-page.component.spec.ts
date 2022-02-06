e8b40007c6d758cea346f43027cfc11a2052812f
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2022-02-05 22:10:15 -0500

    added deleted main-page component

---
+++ client/src/app/pages/main-page/main-page.component.spec.ts
@@ -0,0 +1,61 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialog } from '@angular/material/dialog';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MainPageComponent } from '@app/pages/main-page/main-page.component';
+import { of } from 'rxjs';
+
+export class MatDialogMock {
+    open() {
+        return {
+            afterClosed: () => of({}),
+        };
+    }
+}
+
+describe('MainPageComponent', () => {
+    let component: MainPageComponent;
+    let fixture: ComponentFixture<MainPageComponent>;
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [RouterTestingModule, MatButtonModule, MatCardModule, MatIconModule],
+            declarations: [MainPageComponent],
+            providers: [
+                {
+                    provide: MatDialog,
+                    useClass: MatDialogMock,
+                },
+            ],
+        }).compileComponents();
+    });
+
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MainPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
+
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
+
+    it('openDialog() should open the dialog', () => {
+        const stubParameter = '';
+        const dialogSpy = spyOn(component.dialog, 'open');
+        component.openDialog(stubParameter);
+        expect(dialogSpy).toHaveBeenCalled();
+    });
+    it('The Classic button should pass the Classique game mode', () => {
+        const CLASSIC_GAME_MODE = 'classique';
+        const openDialogSpy = spyOn(component, 'openDialog');
+
+        const button = fixture.debugElement.nativeElement.querySelector('#classique');
+        button.click();
+        expect(openDialogSpy).toHaveBeenCalledWith(CLASSIC_GAME_MODE);
+    });
+    it('A title should exist', () => {
+        expect(component.title).toBeTruthy();
+    });
+});
---
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { DialogBoxComponent } from '@app/components/dialog-box/dialog-box.component';
+
+@Component({
+    selector: 'app-main-page',
+    templateUrl: './main-page.component.html',
+    styleUrls: ['./main-page.component.scss'],
+})
+export class MainPageComponent {
+    readonly title: string = "Bienvenue au jeu Scrabble de l'équipe 107";
+    private readonly dialogWidth: string = '25%';
+    constructor(public dialog: MatDialog) {}
+
+    openDialog(gameModeValue: string): void {
+        this.dialog.open(DialogBoxComponent, {
+            width: this.dialogWidth,
+            data: gameModeValue,
+        });
+    }
+}
