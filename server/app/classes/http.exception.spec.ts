d0340f08749b8da6c6271a6f14c372f92592447c
Mariana Chang Ieong <mariana.8080@hotmail.com>
2022-02-05 22:55:13 -0500

    separated letter and player logic from game logic branch

--- server/app/classes/http.exception.spec.ts
+++
@@ -1,8 +0,0 @@
-// describe('HttpException', () => {
-//     it('should create a simple HTTPException', () => {
-//         const createdMessage = 'Course created successfuly';
-//         const httpException: HttpException = new HttpException(createdMessage);
-
-//         expect(httpException.message).to.equals(createdMessage);
-//     });
-// });
