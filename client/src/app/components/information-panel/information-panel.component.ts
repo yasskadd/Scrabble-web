0389879cfec8ce6f4e8be97327eeb526d658873f
Mariana Chang Ieong <mariana.8080@hotmail.com>
2022-02-14 12:25:47 -0500

    update on player rack view

--- client/src/app/components/information-panel/information-panel.component.ts
+++ client/src/app/components/information-panel/information-panel.component.ts
@@ -13,6 +13,10 @@ export class InformationPanelComponent {
     private readonly dialogWidth: string = '25%';
     constructor(public gameClientService: GameClientService, public dialog: MatDialog, public router: Router) {}
 
+    formatLabel(value: number) {
+        return value + 'px';
+    }
+
     abandonGame(): void {
         this.dialog.open(AbandonGameDialogBoxComponent, {
             width: this.dialogWidth,
