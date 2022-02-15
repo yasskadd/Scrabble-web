70b1f09e4e40135dc481e12065863a8fa5541aa4
LaurentNg <laurentnguyen82@gmail.com>
2022-02-15 01:20:42 -0500

    better turn test

--- server/app/classes/turn.spec.ts
+++ server/app/classes/turn.spec.ts
@@ -7,8 +7,8 @@ import { Turn } from './turn';
 
 const SECOND = 1000;
 
-describe('TurnService', () => {
-    let turnService: Turn;
+describe.only('turn', () => {
+    let turn: Turn;
     let clock: SinonFakeTimers;
     let time: number;
     let player1: Player;
@@ -16,7 +16,7 @@ describe('TurnService', () => {
 
     beforeEach(() => {
         time = 30;
-        turnService = new Turn(time);
+        turn = new Turn(time);
         clock = useFakeTimers();
         player1 = new Player('player1');
         player2 = new Player('player2');
@@ -27,88 +27,88 @@ describe('TurnService', () => {
     });
 
     it('start() should start the timer and not end it when there is still time left on the clock', () => {
-        turnService.start();
-        const endSpy = spy(turnService, 'end');
+        turn.start();
+        const endSpy = spy(turn, 'end');
         clock.tick(SECOND);
         expect(endSpy.called).to.be.false;
     });
 
     it('start() should start the timer and  end it when the time up', () => {
-        turnService.start();
-        const endSpy = spy(turnService, 'end');
+        turn.start();
+        const endSpy = spy(turn, 'end');
         clock.tick(time * SECOND);
         expect(endSpy.called).to.be.true;
     });
 
     it('determinePlayer() should initialize activePlayer and inactivePlayer both different from each other', () => {
-        turnService.determinePlayer(player1, player2);
-        const activePlayer = turnService.activePlayer;
-        const inactivePlayer = turnService.inactivePlayer;
+        turn.determinePlayer(player1, player2);
+        const activePlayer = turn.activePlayer;
+        const inactivePlayer = turn.inactivePlayer;
         expect(activePlayer).to.not.undefined;
         expect(inactivePlayer).to.not.undefined;
         expect(activePlayer).to.not.equal(inactivePlayer);
     });
 
     it('end() should end the activePlayer turn and start the inactivePlayer turn', () => {
-        turnService.activePlayer = player1.name;
-        turnService.inactivePlayer = player2.name;
-        const startSpy = spy(turnService, 'start');
-        // turnService.start();
-        turnService.end();
+        turn.activePlayer = player1.name;
+        turn.inactivePlayer = player2.name;
+        const startSpy = spy(turn, 'start');
+        // turn.start();
+        turn.end();
 
         expect(startSpy.called).to.be.true;
-        expect(turnService.activePlayer).to.equal(player2.name);
-        expect(turnService.inactivePlayer).to.equal(player1.name);
+        expect(turn.activePlayer).to.equal(player2.name);
+        expect(turn.inactivePlayer).to.equal(player1.name);
     });
 
     it('end() should not start anyone turn if true is entered as parameter to signal the ending of the game', () => {
-        turnService.activePlayer = player1.name;
-        turnService.inactivePlayer = player2.name;
-        const spyStart = spy(turnService, 'start');
-        turnService.end(true);
+        turn.activePlayer = player1.name;
+        turn.inactivePlayer = player2.name;
+        const spyStart = spy(turn, 'start');
+        turn.end(true);
 
         expect(spyStart.called).to.be.false;
-        expect(turnService.activePlayer).to.equal(undefined);
+        expect(turn.activePlayer).to.equal(undefined);
     });
 
     it('validating() should return true if it is the turn of the player entered as the parameter to play', () => {
-        turnService.activePlayer = player1.name;
-        turnService.inactivePlayer = player2.name;
-        const validated = turnService.validating(player1.name);
+        turn.activePlayer = player1.name;
+        turn.inactivePlayer = player2.name;
+        const validated = turn.validating(player1.name);
 
         expect(validated).to.be.true;
     });
 
     it('validating() should return false if it is not the turn of the player entered as the parameter to play', () => {
-        turnService.activePlayer = player2.name;
-        turnService.inactivePlayer = player1.name;
-        const validated = turnService.validating(player1.name);
+        turn.activePlayer = player2.name;
+        turn.inactivePlayer = player1.name;
+        const validated = turn.validating(player1.name);
 
         expect(validated).to.be.false;
     });
 
     it('skipTurn() should increment skipCounter by 1', () => {
-        turnService.skipTurn();
+        turn.skipTurn();
 
         // eslint-disable-next-line dot-notation
-        expect(turnService['skipCounter']).to.equal(1);
+        expect(turn['skipCounter']).to.equal(1);
     });
 
     it('skipTurn() should call end with true as argument when the skipCounter is at 6', () => {
-        const endSpy = spy(turnService, 'end');
+        const endSpy = spy(turn, 'end');
         // eslint-disable-next-line dot-notation
-        turnService['skipCounter'] = 5;
-        turnService.skipTurn();
+        turn['skipCounter'] = 5;
+        turn.skipTurn();
 
         // eslint-disable-next-line dot-notation
         expect(endSpy.calledWith(true)).to.be.true;
     });
 
     it('skipTurn() should call end without true as argument when the skipCounter is not at 6', () => {
-        const endSpy = spy(turnService, 'end');
+        const endSpy = spy(turn, 'end');
         // eslint-disable-next-line dot-notation
-        turnService['skipCounter'] = 4;
-        turnService.skipTurn();
+        turn['skipCounter'] = 4;
+        turn.skipTurn();
 
         // eslint-disable-next-line dot-notation
         expect(endSpy.calledWith(true)).to.be.false;
@@ -116,18 +116,18 @@ describe('TurnService', () => {
 
     it('resetSkipCounter() should assign skipCounter to 0', () => {
         // eslint-disable-next-line dot-notation
-        turnService['skipCounter'] = 4;
-        turnService.resetSkipCounter();
+        turn['skipCounter'] = 4;
+        turn.resetSkipCounter();
 
         // eslint-disable-next-line dot-notation
-        expect(turnService['skipCounter']).to.equal(0);
+        expect(turn['skipCounter']).to.equal(0);
     });
 
     it('incrementSkipCounter() should increment skipCounter by 1', () => {
         // eslint-disable-next-line dot-notation
-        turnService['incrementSkipCounter']();
+        turn['incrementSkipCounter']();
 
         // eslint-disable-next-line dot-notation
-        expect(turnService['skipCounter']).to.equal(1);
+        expect(turn['skipCounter']).to.equal(1);
     });
 });
