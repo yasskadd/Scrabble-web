0fe1957ec1101ccbd07cd84e8dd51af292e143b9
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2022-02-15 16:27:29 -0500

    incremented coordinate in word class test suite to match client side

--- server/app/classes/word.class.spec.ts
+++ server/app/classes/word.class.spec.ts
@@ -14,7 +14,7 @@ import { Word } from './word.class';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 // import sinon = require('sinon');
 
-describe('Word', () => {
+describe.only('Word', () => {
     let word: Word;
     const letterA = {} as Letter;
     const letterB = {} as Letter;
@@ -56,7 +56,7 @@ describe('Word', () => {
     });
 
     it('should correctly calculate points if there is no multiplier', () => {
-        const coordList: LetterTile[] = [new LetterTile(0, 4, letterA), new LetterTile(1, 4, letterB), new LetterTile(2, 4, letterC)];
+        const coordList: LetterTile[] = [new LetterTile(1, 5, letterA), new LetterTile(2, 5, letterB), new LetterTile(3, 5, letterC)];
         word = new Word(true, coordList);
         const expectedScore: number = letterA.points + letterB.points + letterC.points;
         const score: number = word.calculatePoints(gameboard);
@@ -64,7 +64,7 @@ describe('Word', () => {
     });
 
     it('should correctly calculate points if there is a letterMultiplier', () => {
-        const coordList: LetterTile[] = [new LetterTile(1, 0, letterA), new LetterTile(2, 0, letterB), new LetterTile(3, 0, letterC)];
+        const coordList: LetterTile[] = [new LetterTile(2, 1, letterA), new LetterTile(3, 1, letterB), new LetterTile(4, 1, letterC)];
         word = new Word(true, coordList);
         const expectedScore: number = letterA.points + letterB.points + letterC.points * 2;
         const score: number = word.calculatePoints(gameboard);
@@ -72,7 +72,7 @@ describe('Word', () => {
     });
 
     it('should correctly calculate points if word is vertical and on letterMultiplier', () => {
-        const coordList: LetterTile[] = [new LetterTile(6, 0, letterA), new LetterTile(6, 1, letterB), new LetterTile(6, 2, letterC)];
+        const coordList: LetterTile[] = [new LetterTile(7, 1, letterA), new LetterTile(7, 2, letterB), new LetterTile(7, 3, letterC)];
         word = new Word(false, coordList);
         const expectedScore: number = letterA.points + letterB.points + letterC.points * 2;
         const score: number = word.calculatePoints(gameboard);
@@ -80,7 +80,7 @@ describe('Word', () => {
     });
 
     it('should correctly calculate points if word is on a word multiplier', () => {
-        const coordList: LetterTile[] = [new LetterTile(0, 0, letterA), new LetterTile(1, 0, letterB), new LetterTile(2, 0, letterC)];
+        const coordList: LetterTile[] = [new LetterTile(1, 1, letterA), new LetterTile(2, 1, letterB), new LetterTile(3, 1, letterC)];
         word = new Word(true, coordList);
         const expectedScore: number = (letterA.points + letterB.points + letterC.points) * 3;
         const score: number = word.calculatePoints(gameboard);
@@ -88,7 +88,7 @@ describe('Word', () => {
     });
 
     it('should correctly calculate points if word is on a word multiplier and is vertical', () => {
-        const coordList: LetterTile[] = [new LetterTile(0, 0, letterA), new LetterTile(0, 1, letterB), new LetterTile(0, 2, letterC)];
+        const coordList: LetterTile[] = [new LetterTile(1, 1, letterA), new LetterTile(1, 2, letterB), new LetterTile(1, 3, letterC)];
         word = new Word(true, coordList);
         const expectedScore: number = (letterA.points + letterB.points + letterC.points) * 3;
         const score: number = word.calculatePoints(gameboard);
@@ -98,10 +98,10 @@ describe('Word', () => {
     it('should correctly calculate points if word is on a Letter and a word multiplier', () => {
         // eslint-disable-next-line max-len
         const coordList: LetterTile[] = [
-            new LetterTile(0, 7, letterA),
-            new LetterTile(1, 7, letterB),
-            new LetterTile(2, 7, letterC),
-            new LetterTile(3, 7, letterD),
+            new LetterTile(1, 8, letterA),
+            new LetterTile(2, 8, letterB),
+            new LetterTile(3, 8, letterC),
+            new LetterTile(4, 8, letterD),
         ];
         word = new Word(true, coordList);
         const expectedScore: number = (letterA.points + letterB.points + letterC.points + letterD.points * 2) * 3;
@@ -110,7 +110,7 @@ describe('Word', () => {
     });
 
     it('should correctly calculate points if word is on two Letter multipliers', () => {
-        const coordList: LetterTile[] = [new LetterTile(6, 6, letterA), new LetterTile(7, 6, letterB), new LetterTile(8, 6, letterC)];
+        const coordList: LetterTile[] = [new LetterTile(7, 7, letterA), new LetterTile(8, 7, letterB), new LetterTile(9, 7, letterC)];
         word = new Word(true, coordList);
         const expectedScore: number = letterA.points * 2 + letterB.points + letterC.points * 2;
         const score: number = word.calculatePoints(gameboard);
@@ -118,25 +118,25 @@ describe('Word', () => {
     });
 
     it('should reset wordMultiplier in gameboard after calculating points', () => {
-        const coordList: LetterTile[] = [new LetterTile(0, 0, letterA), new LetterTile(1, 0, letterB), new LetterTile(2, 0, letterC)];
+        const coordList: LetterTile[] = [new LetterTile(1, 1, letterA), new LetterTile(2, 1, letterB), new LetterTile(3, 1, letterC)];
         word = new Word(true, coordList);
         word.calculatePoints(gameboard);
-        const gameboardCoord = gameboard.getCoord(new LetterTile(0, 0, letterA));
+        const gameboardCoord = gameboard.getCoord(new LetterTile(1, 1, letterA));
         expect(gameboardCoord.wordMultiplier).to.equal(1);
     });
 
     it('should reset Letter in gameboard after calculating points', () => {
-        const coordList: LetterTile[] = [new LetterTile(1, 0, letterA), new LetterTile(2, 0, letterB), new LetterTile(3, 0, letterC)];
+        const coordList: LetterTile[] = [new LetterTile(2, 1, letterA), new LetterTile(3, 1, letterB), new LetterTile(4, 1, letterC)];
         word = new Word(true, coordList);
         word.calculatePoints(gameboard);
-        const gameboardCoord = gameboard.getCoord(new LetterTile(3, 0, letterC));
+        const gameboardCoord = gameboard.getCoord(new LetterTile(4, 1, letterC));
         expect(gameboardCoord.letterMultiplier).to.equal(1);
     });
 
     it('resetLetterMultiplier should be called when points calculation is called', () => {
-        const squareMultiplier3: LetterTile = new LetterTile(3, 0, letterC);
+        const squareMultiplier3: LetterTile = new LetterTile(4, 1, letterC);
         const gameboardCoord: LetterTile = gameboard.getCoord(squareMultiplier3);
-        const coordList: LetterTile[] = [new LetterTile(1, 0, letterA), new LetterTile(2, 0, letterB), squareMultiplier3];
+        const coordList: LetterTile[] = [new LetterTile(2, 1, letterA), new LetterTile(3, 1, letterB), squareMultiplier3];
         const spyReset = Sinon.spy(gameboardCoord, 'resetLetterMultiplier');
         word = new Word(true, coordList);
         word.calculatePoints(gameboard);
@@ -144,9 +144,9 @@ describe('Word', () => {
     });
 
     it('resetWordMultiplier should be called when points calculation is called', () => {
-        const squareMultiplier3: LetterTile = new LetterTile(0, 0, letterA);
+        const squareMultiplier3: LetterTile = new LetterTile(1, 1, letterA);
         const gameboardCoord: LetterTile = gameboard.getCoord(squareMultiplier3);
-        const coordList: LetterTile[] = [squareMultiplier3, new LetterTile(1, 0, letterB), new LetterTile(2, 0, letterC)];
+        const coordList: LetterTile[] = [squareMultiplier3, new LetterTile(2, 1, letterB), new LetterTile(3, 1, letterC)];
         const spyReset = Sinon.spy(gameboardCoord, 'resetWordMultiplier');
         word = new Word(true, coordList);
         word.calculatePoints(gameboard);
