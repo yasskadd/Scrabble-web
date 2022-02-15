a9fd9c57116c3780fc585bc18f42879203d4ae77
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2022-02-14 23:53:42 -0500

    changed constant

--- client/src/app/letter-constants.ts
+++ client/src/app/letter-constants.ts
@@ -1,6 +1,6 @@
 export const FONT_SIZE = 16;
-// eslint-disable-next-line @typescript-eslint/no-magic-numbers
-export const POINTS_FONT_SIZE = FONT_SIZE * 0.45;
+export const POINTS_RATIO = 0.45;
+export const POINTS_FONT_SIZE = FONT_SIZE * POINTS_RATIO;
 export const LETTER_TILE_RATIO = 0.8;
 export const CANVAS_HEIGHT = 75;
 export const CANVAS_WIDTH = 500;
