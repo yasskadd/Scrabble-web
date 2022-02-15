e2985e2d975f8bcfb4cd7a3ea3c9ffa3772e1bf1
Mariana Chang Ieong <mariana.8080@hotmail.com>
2022-02-15 03:35:54 -0500

    removed useless sidebar (prof example)

--- client/src/app/pages/game-page/game-page.component.spec.ts
+++ client/src/app/pages/game-page/game-page.component.spec.ts
@@ -1,6 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PlayAreaComponent } from '@app/components/play-area/play-area.component';
-import { SidebarComponent } from '@app/components/sidebar/sidebar.component';
 import { GamePageComponent } from './game-page.component';
 
 describe('GamePageComponent', () => {
@@ -9,7 +8,7 @@ describe('GamePageComponent', () => {
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
-            declarations: [GamePageComponent, SidebarComponent, PlayAreaComponent],
+            declarations: [GamePageComponent, PlayAreaComponent],
         }).compileComponents();
     });
 
