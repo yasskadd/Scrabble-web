a44317ee7727bfca6479d00916f45579cab0bf6c
Mariana Chang Ieong <mariana.8080@hotmail.com>
2022-02-24 23:54:58 -0500

    fixed tiles size adjustments - much smoother now

--- client/src/app/components/information-panel/information-panel.component.ts
+++ client/src/app/components/information-panel/information-panel.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
+import { MatSliderChange } from '@angular/material/slider';
 import { Router } from '@angular/router';
 import { AbandonGameDialogBoxComponent } from '@app/components/abandon-game-dialog-box/abandon-game-dialog-box.component';
 import { GameClientService } from '@app/services/game-client.service';
@@ -20,8 +21,8 @@ export class InformationPanelComponent {
         return value + 'px';
     }
 
-    updateFontSize(): void {
-        this.gridService.letterSize = this.value;
+    updateFontSize(event: MatSliderChange): void {
+        this.gridService.letterSize = event.value as number;
         this.gameClientService.updateGameboard();
     }
 
