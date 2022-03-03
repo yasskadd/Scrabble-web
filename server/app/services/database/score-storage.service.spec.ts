c6aa2117e047cfa0de105a7f921794d1cb1d6ea3
Vincent Richard <vincent.richard.01@gmail.com>
2022-03-03 13:21:49 -0500

    test finish for high-score feature

--- server/app/services/database/score-storage.service.spec.ts
+++ server/app/services/database/score-storage.service.spec.ts
@@ -21,6 +21,38 @@ const TOP_SCORES = [
     },
 ];
 
+const TOP_SCORES_CLASSIC = [
+    {
+        username: 'Paul',
+        type: 'Classique',
+        score: 0,
+        position: 1,
+    },
+    {
+        username: 'MARC',
+        type: 'Classique',
+        score: 20,
+        position: 2,
+    },
+    {
+        username: 'Luc',
+        type: 'Classique',
+        score: 30,
+        position: 3,
+    },
+    {
+        username: 'Jean',
+        type: 'Classique',
+        score: 600,
+        position: 4,
+    },
+    {
+        username: 'Jules',
+        type: 'Classique',
+        score: 500000000,
+        position: 5,
+    },
+];
 describe('scoreStorage Service', () => {
     let databaseServiceStub: Sinon.SinonStubbedInstance<DatabaseService>;
     let scoreStorageService: ScoreStorageService;
@@ -52,6 +84,22 @@ describe('scoreStorage Service', () => {
         const scoresClassic = scoreStorageService['getTopScoresPosition'](TOP_SCORES, TEST_SCORE);
         // eslint-disable-next-line dot-notation
         expect(scoresClassic).to.be.deep.equal(EXPECTED_POSTITION);
+    });
+
+    it('isNameAlreadyThere() should return true if the position in the ranking already has the name of the player', () => {
+        const PLAYER_NAME = 'Paul';
+        // eslint-disable-next-line dot-notation
+        const playerNameAlready = scoreStorageService['isNameAlreadyThere'](TOP_SCORES_CLASSIC[0].username, PLAYER_NAME);
+        // eslint-disable-next-line dot-notation
+        expect(playerNameAlready).to.be.deep.equal(true);
+    });
+
+    it('isNameAlreadyThere() should return false if the position in the ranking does not already has the name of the player', () => {
+        const PLAYER_NAME = 'Paul';
+        // eslint-disable-next-line dot-notation
+        const playerNameAlready = scoreStorageService['isNameAlreadyThere'](TOP_SCORES_CLASSIC[1].username, PLAYER_NAME);
+        // eslint-disable-next-line dot-notation
+        expect(playerNameAlready).to.be.deep.equal(false);
     });
     it("populateDb() should populate the database if it's not populated", async () => {
         databaseServiceStub.fetchDocuments.resolves([]);
@@ -86,12 +134,66 @@ describe('scoreStorage Service', () => {
         Sinon.stub(scoreStorageService, 'populateDb' as never);
         const topScoresPositionStub = Sinon.stub(scoreStorageService, 'getTopScoresPosition' as never);
         topScoresPositionStub.returns(POSITION);
-        databaseServiceStub.fetchDocuments.resolves(TOP_SCORES);
+        databaseServiceStub.fetchDocuments.resolves(TOP_SCORES_CLASSIC);
         databaseServiceStub.replaceDocument.resolves();
         // eslint-disable-next-line dot-notation
         await scoreStorageService['addTopScores'](scoreInfo);
         // eslint-disable-next-line dot-notation
         expect(databaseServiceStub.replaceDocument.withArgs({ position: POSITION }, { ...scoreInfo, position: POSITION }).callCount).to.be.equal(1);
+    });
+
+    it('addTopScores() should add the name of the player if he has the same score of someone else ', async () => {
+        const POSITION = 3;
+        const scoreInfo = {
+            username: 'Luc',
+            type: 'Classique',
+            score: 30,
+        };
+        const infoPlayer = {
+            type: 'Classique',
+            score: 30,
+        };
+        Sinon.stub(scoreStorageService, 'populateDb' as never);
+        const topScoresPositionStub = Sinon.stub(scoreStorageService, 'getTopScoresPosition' as never);
+        topScoresPositionStub.returns(POSITION);
+        databaseServiceStub.fetchDocuments.resolves(TOP_SCORES_CLASSIC);
+        databaseServiceStub.replaceDocument.resolves();
+        // eslint-disable-next-line dot-notation
+        await scoreStorageService['addTopScores'](scoreInfo);
+        // eslint-disable-next-line dot-notation
+        expect(
+            databaseServiceStub.replaceDocument.withArgs(
+                { position: POSITION },
+                { ...infoPlayer, username: TOP_SCORES_CLASSIC[2].username + ' - ' + scoreInfo.username, position: POSITION },
+            ).callCount,
+        ).to.be.equal(0);
+    });
+
+    it('addTopScores() should not add the name of the player if he did already in the leaderboard at this position ', async () => {
+        const POSITION = 3;
+        const scoreInfo = {
+            username: 'Paul',
+            type: 'Classique',
+            score: 30,
+        };
+        const infoPlayer = {
+            type: 'Classique',
+            score: 30,
+        };
+        Sinon.stub(scoreStorageService, 'populateDb' as never);
+        const topScoresPositionStub = Sinon.stub(scoreStorageService, 'getTopScoresPosition' as never);
+        topScoresPositionStub.returns(POSITION);
+        databaseServiceStub.fetchDocuments.resolves(TOP_SCORES_CLASSIC);
+        databaseServiceStub.replaceDocument.resolves();
+        // eslint-disable-next-line dot-notation
+        await scoreStorageService['addTopScores'](scoreInfo);
+        // eslint-disable-next-line dot-notation
+        expect(
+            databaseServiceStub.replaceDocument.withArgs(
+                { position: POSITION },
+                { ...infoPlayer, username: TOP_SCORES_CLASSIC[2].username + ' - ' + scoreInfo.username, position: POSITION },
+            ).callCount,
+        ).to.be.equal(1);
     });
     it("addTopScores() shouldn't replace a top score with the score info if it's illegible ", async () => {
         const POSITION = 6;
