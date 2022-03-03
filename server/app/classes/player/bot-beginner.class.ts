ac3635d2f4e99cf136f11f8833ee24c6c5ed4ae2
yasskadd <71228349+yasskadd@users.noreply.github.com>
2022-03-03 13:51:48 -0500

    implemented placeLetter and exchangeLetter for bot-beginner-class

--- server/app/classes/player/bot-beginner.class.ts
+++ server/app/classes/player/bot-beginner.class.ts
@@ -1,6 +1,9 @@
+import { Gameboard } from '@app/classes/gameboard.class';
 import { CommandInfo } from '@app/command-info';
 import { Game } from '@app/services/game.service';
 import { Player } from './player.class';
+
+const MAX_NUMBER = 10;
 
 export class BeginnerBot extends Player {
     game: Game;
@@ -13,35 +16,43 @@ export class BeginnerBot extends Player {
     }
 
     /* Take a random number of letters to exchange (between 1 and length of rack)
-        and take random indexes from the player rack in order to exchange them */
-    exchangeLetter(letters: string[]) {
+        and take random indexes from the player rack in order to exchange them, if there is not enough letter
+        in the reserve, skipTurn() */
+    exchangeLetter(): void {
         if (this.game === undefined) return;
-        const rack: string[] = [...this.rack];
-        const numberOfLetters = this.getRandomNumber(rack.length);
-        this.rack = this.game.exchange(letters, this);
+        const rack: string[] = [...this.rackToString()];
+        let numberOfLetters = this.getRandomNumber(rack.length);
+        const lettersToExchange: string[] = new Array(numberOfLetters);
+        while (numberOfLetters > 0) {
+            lettersToExchange.push(rack.splice(this.getRandomNumber(rack.length), 1)[0]);
+            numberOfLetters--;
+        }
+        this.rack = this.game.exchange(lettersToExchange, this);
     }
-    skipTurn() {
+
+    skipTurn(): void {
         if (this.game === undefined) return;
         this.game.skip(this.name);
     }
 
+    /* What to do if there is no commandInfo associated with the score range */
     placeLetter(commandInfoMap: Map<CommandInfo, number>): [boolean, Gameboard] | string {
         if (this.game === undefined) return 'error';
         // find the commandInfo based on probability
-        const randomNumber: number = this.getRandomNumber();
+        const randomNumber: number = this.getRandomNumber(MAX_NUMBER);
         const commandInfoList: CommandInfo[] = new Array();
         if (this.inRange(randomNumber, 1, 4)) {
             // maybe a better way to do this using spread operator with condition ?
             commandInfoMap.forEach((value, key) => {
-                if (this.inRange(value, 1, 4)) commandInfoList.push(key);
+                if (this.inRange(value, 1, 6)) commandInfoList.push(key);
             });
         } else if (this.inRange(randomNumber, 5, 7)) {
             commandInfoMap.forEach((value, key) => {
-                if (this.inRange(value, 5, 7)) commandInfoList.push(key);
+                if (this.inRange(value, 7, 12)) commandInfoList.push(key);
             });
         } else {
             commandInfoMap.forEach((value, key) => {
-                if (this.inRange(value, 8, 10)) commandInfoList.push(key);
+                if (this.inRange(value, 13, 18)) commandInfoList.push(key);
             });
         }
         // chose a random commandInfo
@@ -49,11 +60,11 @@ export class BeginnerBot extends Player {
         return this.game.play(this, randomCommandInfo);
     }
 
-    private getRandomNumber(maxNumber: number) {
+    private getRandomNumber(maxNumber: number): number {
         return Math.floor(Math.random() * maxNumber + 1);
     }
 
-    private inRange(x: number, start: number, end: number) {
+    private inRange(x: number, start: number, end: number): boolean {
         return (x - start) * (x - end) <= 0;
     }
 }
