df632b313de6567c29a1130d85a0808491daf93c
Vincent Richard <vincent.richard.01@gmail.com>
2022-03-06 21:52:02 -0500

    start solving problem

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -48,6 +49,7 @@ import { WaitingOpponentPageComponent } from './pages/waiting-opponent-page/wait
         HighScoresComponent,
     ],
     imports: [
+        CommonModule,
         AppMaterialModule,
         AppRoutingModule,
         MatSnackBarModule,
