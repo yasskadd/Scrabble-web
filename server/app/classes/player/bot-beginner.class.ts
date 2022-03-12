df0a4749c48061cc6ec555d3343959a8f9d652f2
yasskadd <71228349+yasskadd@users.noreply.github.com>
2022-03-12 14:57:21 -0500

    refactor bot-beginner class

--- server/app/classes/player/bot-beginner.class.ts
+++ server/app/classes/player/bot-beginner.class.ts
@@ -1,29 +1,41 @@
 import { Gameboard } from '@app/classes/gameboard.class';
-import { CommandInfo } from '@app/command-info';
+import { CommandInfo } from '@app/interfaces/command-info';
 import { Game } from '@app/services/game.service';
 import { SocketManager } from '@app/services/socket-manager.service';
-import { SocketEvents } from '@common/socket-events';
+import { WordSolverService } from '@app/services/word-solver.service';
+import { SocketEvents } from '@common/constants/socket-events';
+import { Inject } from 'typedi';
 import { Player } from './player.class';
 
 const MAX_NUMBER = 10;
 const CHAR_ASCII = 96;
+const RANGE_6 = 6;
+const RANGE_7 = 7;
+const RANGE_12 = 12;
+const RANGE_13 = 13;
+const RANGE_18 = 18;
+const PROB_4 = 4;
+const PROB_5 = 5;
+const PROB_7 = 7;
+
+interface BotInformation {
+    game: Game;
+    socketManager: SocketManager;
+    roomId: string;
+}
 
 export class BeginnerBot extends Player {
-    game: Game;
     isPlayerOne: boolean;
-    roomId: string;
-    constructor(game: Game, isPlayerOne: boolean, name: string, private socketManager: SocketManager, roomId: string) {
+    constructor(@Inject() private wordSolver: WordSolverService, isPlayerOne: boolean, name: string, private botInfo: BotInformation) {
         super(name);
-        this.game = game;
         this.isPlayerOne = isPlayerOne;
-        this.roomId = roomId;
     }
 
-    choosePlayMove(commandInfoMap: Map<CommandInfo, number>) {
+    choosePlayMove() {
         const randomNumber = this.getRandomNumber(MAX_NUMBER);
-        if (this.inRange(randomNumber, 1, 1) || !commandInfoMap.size) this.skipTurn();
+        if (this.inRange(randomNumber, 1, 1)) this.skipTurn();
         else if (this.inRange(randomNumber, 2, 2)) this.exchangeLetter();
-        else this.placeLetter(commandInfoMap);
+        else this.placeLetter();
     }
 
     /* Take a random number of letters to exchange (between 1 and length of rack)
@@ -38,47 +50,50 @@ export class BeginnerBot extends Player {
             lettersToExchange.push(rack.splice(this.getRandomNumber(rack.length), 1)[0]);
             numberOfLetters--;
         }
-        this.socketManager.emitRoom(this.roomId, SocketEvents.GameMessage, `!echanger ${lettersToExchange} lettres`);
+        this.botInfo.socketManager.emitRoom(this.botInfo.roomId, SocketEvents.GameMessage, `!echanger ${lettersToExchange} lettres`);
         this.rack = this.game.exchange(lettersToExchange, this);
     }
 
     skipTurn(): void {
         if (this.game === undefined) return;
-        this.socketManager.emitRoom(this.roomId, SocketEvents.GameMessage, '!passer');
+        this.botInfo.socketManager.emitRoom(this.botInfo.roomId, SocketEvents.GameMessage, '!passer');
         this.game.skip(this.name);
     }
 
     /* What to do if there is no commandInfo associated with the score range */
-    placeLetter(commandInfoMap: Map<CommandInfo, number>): [boolean, Gameboard] | string {
+    placeLetter(): [boolean, Gameboard] | string {
         if (this.game === undefined) return 'error';
-        // find the commandInfo based on probability
+        const commandInfoMap = this.wordSolver.commandInfoScore(this.wordSolver.findAllOptions(this.rackToString()));
         const randomNumber: number = this.getRandomNumber(MAX_NUMBER);
         const commandInfoList: CommandInfo[] = new Array();
-        if (this.inRange(randomNumber, 1, 4)) {
+        this.addCommandInfoToList(commandInfoMap, commandInfoList, randomNumber);
+        const randomCommandInfo = commandInfoList[Math.floor(Math.random() * commandInfoList.length)];
+        this.emitPlaceCommand(randomCommandInfo);
+        return this.game.play(this, randomCommandInfo);
+    }
+
+    private addCommandInfoToList(commandInfoMap: Map<CommandInfo, number>, commandInfoList: CommandInfo[], randomNumber: number) {
+        if (this.inRange(randomNumber, 1, PROB_4)) {
             // maybe a better way to do this using spread operator with condition ?
             commandInfoMap.forEach((value, key) => {
-                if (this.inRange(value, 1, 6)) commandInfoList.push(key);
+                if (this.inRange(value, 1, RANGE_6)) commandInfoList.push(key);
             });
-        } else if (this.inRange(randomNumber, 5, 7)) {
+        } else if (this.inRange(randomNumber, PROB_5, PROB_7)) {
             commandInfoMap.forEach((value, key) => {
-                if (this.inRange(value, 7, 12)) commandInfoList.push(key);
+                if (this.inRange(value, RANGE_7, RANGE_12)) commandInfoList.push(key);
             });
         } else {
             commandInfoMap.forEach((value, key) => {
-                if (this.inRange(value, 13, 18)) commandInfoList.push(key);
+                if (this.inRange(value, RANGE_13, RANGE_18)) commandInfoList.push(key);
             });
         }
-        // chose a random commandInfo
-        const randomCommandInfo = commandInfoList[Math.floor(Math.random() * commandInfoList.length)];
-        this.socketManager.emitRoom(
-            this.roomId,
-            SocketEvents.GameMessage,
-            `!placer ${String.fromCharCode(CHAR_ASCII + randomCommandInfo.firstCoordinate.y)}${randomCommandInfo.firstCoordinate.x}${
-                randomCommandInfo.direction
-            } 
-            ${randomCommandInfo.lettersPlaced}`,
-        );
-        return this.game.play(this, randomCommandInfo);
+        return commandInfoList;
+    }
+
+    private emitPlaceCommand(randomCommandInfo: CommandInfo) {
+        const coordString = ` ${String.fromCharCode(CHAR_ASCII + randomCommandInfo.firstCoordinate.y)}${randomCommandInfo.firstCoordinate.x}`;
+        const placeCommand = `${coordString}${randomCommandInfo.direction} ${randomCommandInfo.lettersPlaced}`;
+        this.botInfo.socketManager.emitRoom(this.botInfo.roomId, SocketEvents.GameMessage, placeCommand);
     }
 
     private getRandomNumber(maxNumber: number): number {
