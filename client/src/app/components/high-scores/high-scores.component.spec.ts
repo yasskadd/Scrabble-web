50eced4238dbc6fd7510ebb31834e1afd2f4d65d
vincentRichardd <vincent.richard.01@gmail.com>
2022-03-14 17:50:05 -0400

    highScore test refactor

--- client/src/app/components/high-scores/high-scores.component.spec.ts
+++ client/src/app/components/high-scores/high-scores.component.spec.ts
@@ -2,13 +2,22 @@ import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { HttpHandlerService } from '@app/services/http-handler.service';
 import { of } from 'rxjs';
 import { HighScoresComponent } from './high-scores.component';
+
+const TEST_ERROR = 'Requête Impossible a faire au serveur';
+
 describe('HighScoresComponent', () => {
     let component: HighScoresComponent;
     let fixture: ComponentFixture<HighScoresComponent>;
     let httpHandlerSpy: jasmine.SpyObj<HttpHandlerService>;
+    let matSnackBar: MatSnackBar;
+    const mockMatSnackBar = {
+        // eslint-disable-next-line @typescript-eslint/no-empty-function
+        open: () => {},
+    };
 
     beforeEach(async () => {
         httpHandlerSpy = jasmine.createSpyObj('HttpHandlerService', ['getClassicHighScore', 'getLOG2990HighScore']);
@@ -21,7 +30,10 @@ describe('HighScoresComponent', () => {
         await TestBed.configureTestingModule({
             imports: [HttpClientModule, MatCardModule, MatIconModule],
             declarations: [HighScoresComponent],
-            providers: [{ provide: HttpHandlerService, useValue: httpHandlerSpy }],
+            providers: [
+                { provide: HttpHandlerService, useValue: httpHandlerSpy },
+                { provide: MatSnackBar, useValue: mockMatSnackBar },
+            ],
         }).compileComponents();
     });
 
@@ -29,6 +41,7 @@ describe('HighScoresComponent', () => {
         fixture = TestBed.createComponent(HighScoresComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
+        matSnackBar = TestBed.inject(MatSnackBar);
     });
 
     it('should create', () => {
@@ -39,5 +52,17 @@ describe('HighScoresComponent', () => {
         component.getHighScores();
         expect(httpHandlerSpy.getClassicHighScore).toHaveBeenCalled();
         expect(httpHandlerSpy.getLOG2990HighScore).toHaveBeenCalled();
+    });
+
+    it('openSnackBar should call the MatSnackBar open method', () => {
+        const matSnackBarSpy = spyOn(matSnackBar, 'open').and.stub();
+        component.openSnackBar(TEST_ERROR);
+        expect(matSnackBarSpy.calls.count()).toBe(1);
+        const args = matSnackBarSpy.calls.argsFor(0);
+        expect(args[0]).toBe(TEST_ERROR);
+        expect(args[1]).toBe('fermer');
+        expect(args[2]).toEqual({
+            verticalPosition: 'top',
+        });
     });
 });
--- client/src/app/components/high-scores/high-scores.component.ts
+++ client/src/app/components/high-scores/high-scores.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
 import { HighScores } from '@app/interfaces/high-score-parameters';
 import { HttpHandlerService } from '@app/services/http-handler.service';
 
@@ -10,14 +11,23 @@ import { HttpHandlerService } from '@app/services/http-handler.service';
 export class HighScoresComponent implements OnInit {
     highScoreClassic: HighScores[];
     highScoreLOG29990: HighScores[];
-    constructor(private readonly httpHandler: HttpHandlerService) {}
+    constructor(private readonly httpHandler: HttpHandlerService, public snackBar: MatSnackBar) {}
 
     ngOnInit(): void {
         this.getHighScores();
     }
 
     getHighScores() {
-        this.httpHandler.getClassicHighScore().subscribe((highScore) => (this.highScoreClassic = highScore));
+        this.httpHandler.getClassicHighScore().subscribe(
+            (highScore) => (this.highScoreClassic = highScore),
+            () => this.openSnackBar('Requête Impossible a faire au serveur'),
+        );
         this.httpHandler.getLOG2990HighScore().subscribe((highScore) => (this.highScoreLOG29990 = highScore));
+    }
+
+    openSnackBar(reason: string): void {
+        this.snackBar.open(reason, 'fermer', {
+            verticalPosition: 'top',
+        });
     }
 }
