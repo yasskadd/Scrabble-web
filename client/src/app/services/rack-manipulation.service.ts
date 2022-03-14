3b9238d83b9cae4bcfcf79f7bb8ddb2e0690695e
Mariana Chang Ieong <mariana.8080@hotmail.com>
2022-03-14 12:42:26 -0400

    quick changes on rackmanipulation

--- client/src/app/services/rack-manipulation.service.ts
+++ client/src/app/services/rack-manipulation.service.ts
@@ -5,9 +5,10 @@ import * as constants from '@app/constants';
     providedIn: 'root',
 })
 export class RackManipulationService {
-    width = constants.RACK_WIDTH;
-    height = constants.RACK_HEIGHT;
-    buttonPressed = '';
+    width: number;
+    height: number;
+    buttonPressed: string;
+
     currentSelection = 0;
     previousSelection = constants.INVALID_INDEX;
     previousScrollPosition = 0;
@@ -16,7 +17,54 @@ export class RackManipulationService {
     duplicates: number[] = [];
     arrow: boolean = false;
     absentFromRack: boolean = true;
-    constructor() {}
+
+    constructor() {
+        this.width = constants.RACK_WIDTH;
+        this.height = constants.RACK_HEIGHT;
+        this.buttonPressed = '';
+    }
+
+    cancel() {
+        this.lettersToExchange = [];
+        this.lettersToManipulate = [];
+    }
+
+    // repositionRack() {
+    //     this.previousSelection = constants.INVALID_INDEX;
+    //     if (this.buttonPressed === 'ArrowLeft') {
+    //         this.moveLeft();
+    //     } else {
+    //         this.moveRight();
+    //     }
+    // }
+
+    // selectManipulation(event: KeyboardEvent) {
+    //     this.duplicates = [];
+    //     this.arrow = this.buttonPressed === 'ArrowLeft' || this.buttonPressed === 'ArrowRight';
+
+    //     if (this.arrow) {
+    //         this.repositionRack();
+    //         return;
+    //     }
+
+    //     for (const [i, letter] of this.rack.entries()) {
+    //         if (letter.value === event.key.toLowerCase()) {
+    //             this.duplicates.push(i);
+    //         }
+    //     }
+
+    //     if (this.duplicates.length === 1) {
+    //         this.previousSelection = constants.INVALID_INDEX;
+    //         this.currentSelection = this.duplicates[0];
+    //     } else if (this.duplicates.length > 1) {
+    //         this.currentSelection = this.duplicates[(this.duplicates.indexOf(this.currentSelection) + 1) % this.duplicates.length];
+    //         this.previousSelection = this.currentSelection;
+    //     } else {
+    //         this.cancel();
+    //         return;
+    //     }
+    //     this.lettersToManipulate.push(this.currentSelection);
+    // }
 
     selectExchange(event: MouseEvent, letter: number) {
         event.preventDefault();
@@ -30,5 +78,12 @@ export class RackManipulationService {
                 this.lettersToExchange.splice(index, 1);
             }
         }
+    }
+
+    onLeftClick(event: MouseEvent, letter: number) {
+        event.preventDefault();
+        this.cancel();
+        this.currentSelection = letter;
+        this.lettersToManipulate.push(letter);
     }
 }
