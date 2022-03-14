d1e0e4d904c5e8e66fe93923b646c99192b31284
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2022-03-14 11:42:41 -0400

    dictionary validation tests pass

--- server/app/services/dictionary-validation.service.spec.ts
+++ server/app/services/dictionary-validation.service.spec.ts
@@ -12,9 +12,9 @@ import * as fs from 'fs';
 import * as Sinon from 'sinon';
 import { DictionaryValidationService } from './dictionary-validation.service';
 
-const jsonDictionary = JSON.parse(fs.readFileSync('@app/../assets/dictionary.json', 'utf8'));
+const jsonDictionary = JSON.parse(fs.readFileSync('./assets/dictionary.json', 'utf8'));
 
-describe('Dictionary Validation Service', () => {
+describe.only('Dictionary Validation Service', () => {
     let dictionaryValidationService: DictionaryValidationService;
     let gameboard: Gameboard;
 
@@ -110,7 +110,11 @@ describe('Dictionary Validation Service', () => {
     });
 
     it('should return 0 points when validateWord() is called and wordList is invalid', () => {
-        expect(dictionaryValidationService.validateWord(invalidWord1, gameboard)).to.equal(0);
+        expect(dictionaryValidationService.validateWord(invalidWord1, gameboard).points).to.equal(0);
+    });
+
+    it('should return array with invalidWord when validateWord() is called and wordList is invalid', () => {
+        expect(dictionaryValidationService.validateWord(invalidWord1, gameboard).invalidWords[0]).to.eql(invalidWord1);
     });
 
     context('Trie Dictionary tests', () => {
