fce2e9c3f9e01a074811e68ac24d2376a2b49f2d
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2022-03-15 21:53:22 -0400

    server client and tests build

--- server/app/classes/letter-tile.class.spec.ts
+++ server/app/classes/letter-tile.class.spec.ts
@@ -1,6 +1,6 @@
 /* eslint-disable prettier/prettier */
 /* eslint-disable @typescript-eslint/no-magic-numbers */
-import { LetterTile } from '@common/letter-tile.class';
+import { LetterTile } from '@common/classes/letter-tile.class';
 import { expect } from 'chai';
 
 describe('LetterTile', () => {
