4caae9d04d2559a73555237fc8dfa64b31d3c445
esmegenereux <71102910+esmegenereux@users.noreply.github.com>
2022-03-28 15:46:06 -0400

    admin-dictionaries same format as admin-virtual-player

--- client/src/app/components/admin-dictionaries/admin-dictionaries.component.ts
+++ client/src/app/components/admin-dictionaries/admin-dictionaries.component.ts
@@ -1,12 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+interface Dictionary {
+    title: string;
+    description: string;
+}
+
+const defaultDict: Dictionary = {
+    title: 'default',
+    description: 'Default Dictionary',
+};
+
 @Component({
     selector: 'app-admin-dictionaries',
     templateUrl: './admin-dictionaries.component.html',
     styleUrls: ['./admin-dictionaries.component.scss'],
 })
 export class AdminDictionariesComponent implements OnInit {
-    dictionaries: [];
+    dictionaries: Dictionary[] = [defaultDict];
     constructor() {}
 
     ngOnInit(): void {}
