25ca61709ec3819ab4aceedd272177c32708863c
Mariana Chang Ieong <mariana.8080@hotmail.com>
2022-03-30 15:12:57 -0400

    fixing console bugs with NO_ERRORS_SCHEMA

--- client/src/app/components/admin-dictionaries/admin-dictionaries.component.spec.ts
+++ client/src/app/components/admin-dictionaries/admin-dictionaries.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminDictionariesComponent } from './admin-dictionaries.component';
 
@@ -8,6 +9,7 @@ describe('AdminDictionariesComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [AdminDictionariesComponent],
+            schemas: [NO_ERRORS_SCHEMA],
         }).compileComponents();
     });
 
--- client/src/app/components/admin-virtual-players/admin-virtual-players.component.spec.ts
+++ client/src/app/components/admin-virtual-players/admin-virtual-players.component.spec.ts
@@ -1,3 +1,4 @@
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AdminVirtualPlayersComponent } from './admin-virtual-players.component';
 
@@ -8,6 +9,7 @@ describe('AdminVirtualPlayersComponent', () => {
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             declarations: [AdminVirtualPlayersComponent],
+            schemas: [NO_ERRORS_SCHEMA],
         }).compileComponents();
     });
 
