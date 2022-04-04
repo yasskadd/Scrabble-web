41b52dab2b86347667bbe61b40314d2341695360
yasskadd <yasser.kadimi@gmail.com>
2022-04-04 11:31:26 -0400

    added method that verifies all player objectives related to words formed

--- server/app/services/public-objectives.service.ts
+++ server/app/services/public-objectives.service.ts
@@ -12,32 +12,32 @@ export class ObjectivesHandler {
 
     constructor(player1: Player, player2: Player) {
         this.players = [player1, player2];
+        this.setMapObjectives();
+        this.attributeObjectives(player1, player2);
     }
 
-    completePublicObjective() {}
+    verifyObjectives(allWordsFormed: Word[], player: Player): void {
+        player.objectives.forEach((objective) => {
+            const objectiveVerificationFunction = this.objectivesMap.get(objective) as CallableFunction;
+            if (objectiveVerificationFunction(allWordsFormed)) this.addObjectivePoints(player, objective);
+        });
+    }
 
-    completePrivateObjective() {}
-
-    attributeObjectives(player1: Player, player2: Player) {
+    attributeObjectives(player1: Player, player2: Player): void {
         // Set public and private objectives
-        const publicObjective1 = ObjectivesInfo.objectivesList.splice(Math.floor(Math.random() * ObjectivesInfo.objectivesList.length), 1);
-        const publicObjective2 = ObjectivesInfo.objectivesList.splice(Math.floor(Math.random() * ObjectivesInfo.objectivesList.length), 1);
-        const privateObjective1 = ObjectivesInfo.objectivesList.splice(Math.floor(Math.random() * ObjectivesInfo.objectivesList.length), 1);
-        const privateObjectivE2 = ObjectivesInfo.objectivesList.splice(Math.floor(Math.random() * ObjectivesInfo.objectivesList.length), 1);
+        const publicObjective1 = ObjectivesInfo.objectivesList.splice(Math.floor(Math.random() * ObjectivesInfo.objectivesList.length), 1)[0];
+        publicObjective1.isPublic = true;
+        const publicObjective2 = ObjectivesInfo.objectivesList.splice(Math.floor(Math.random() * ObjectivesInfo.objectivesList.length), 1)[0];
+        publicObjective2.isPublic = true;
+        const privateObjective1 = ObjectivesInfo.objectivesList.splice(Math.floor(Math.random() * ObjectivesInfo.objectivesList.length), 1)[0];
+        privateObjective1.isPublic = false;
+        const privateObjectivE2 = ObjectivesInfo.objectivesList.splice(Math.floor(Math.random() * ObjectivesInfo.objectivesList.length), 1)[0];
+        privateObjectivE2.isPublic = false;
         player1.objectives.push(publicObjective1, publicObjective2, privateObjective1);
         player2.objectives.push(publicObjective1, publicObjective2, privateObjectivE2);
     }
 
-    setMapObjectives() {
-        this.objectivesMap.set(ObjectivesInfo.oneVowelWord, this.isWordWithOneVowel);
-        this.objectivesMap.set(ObjectivesInfo.palindromicWord, this.isPalindromicWord);
-        this.objectivesMap.set(ObjectivesInfo.alphabeticalWord, this.isWordAlphabetical);
-        this.objectivesMap.set(ObjectivesInfo.moreThan10Letters, this.isWordMoreThan10Letters);
-        this.objectivesMap.set(ObjectivesInfo.threeWordsFormed, this.isThreeWordsFormed);
-        this.objectivesMap.set(ObjectivesInfo.twoSameWords, this.isSameWordTwoTimes);
-    }
-
-    isWordWithOneVowel(word: Word) {
+    private isWordWithOneVowel(word: Word): boolean {
         if (word.stringFormat.length < MINIMUM_LETTERS_ONE_VOWEL) return false;
         const vowels = ['a', 'e', 'i', 'o', 'u'];
         let vowelCount = 0;
@@ -48,28 +48,42 @@ export class ObjectivesHandler {
         return vowelCount === 1;
     }
 
-    isPalindromicWord(word: Word) {
+    private isPalindromicWord(word: Word): boolean {
         if (word.stringFormat.length < 1) return false;
         const wordArray = word.stringFormat.split('');
         return wordArray === wordArray.reverse();
     }
 
-    isWordAlphabetical(word: Word) {
+    private isWordAlphabetical(word: Word): boolean {
         return word.stringFormat === word.stringFormat.split('').sort().join('');
     }
 
-    isWordMoreThan10Letters(word: Word) {
+    private isWordMoreThan10Letters(word: Word): boolean {
         return word.stringFormat.length > MINIMUM_LETTERS_10;
     }
 
-    isThreeWordsFormed(allWords: Word[]) {
+    private isThreeWordsFormed(allWords: Word[]): boolean {
         return allWords.length > 2;
     }
 
-    isSameWordTwoTimes(allWords: Word[]) {
+    private isSameWordTwoTimes(allWords: Word[]): boolean {
         allWords.forEach((word: Word) => {
             word.stringFormat.toLowerCase();
         });
         return allWords.length !== new Set(allWords).size;
+    }
+
+    private setMapObjectives(): void {
+        this.objectivesMap.set(ObjectivesInfo.oneVowelWord, this.isWordWithOneVowel);
+        this.objectivesMap.set(ObjectivesInfo.palindromicWord, this.isPalindromicWord);
+        this.objectivesMap.set(ObjectivesInfo.alphabeticalWord, this.isWordAlphabetical);
+        this.objectivesMap.set(ObjectivesInfo.moreThan10Letters, this.isWordMoreThan10Letters);
+        this.objectivesMap.set(ObjectivesInfo.threeWordsFormed, this.isThreeWordsFormed);
+        this.objectivesMap.set(ObjectivesInfo.twoSameWords, this.isSameWordTwoTimes);
+    }
+
+    private addObjectivePoints(player: Player, objective: Objective): void {
+        player.score += objective.points;
+        player.score *= objective.pointsMultiplier;
     }
 }
