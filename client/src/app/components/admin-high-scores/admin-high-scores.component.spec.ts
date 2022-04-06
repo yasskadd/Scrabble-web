639893d3af130d87aa9eed661efed4b5a6c7f7c8
Mariana Chang Ieong <mariana.8080@hotmail.com>
2022-04-06 17:40:06 -0400

    all tests pass - only needing more coverage

--- client/src/app/components/admin-high-scores/admin-high-scores.component.spec.ts
+++ client/src/app/components/admin-high-scores/admin-high-scores.component.spec.ts
@@ -1,13 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HighScoresService } from '@app/services/high-scores.service';
 import { AdminHighScoresComponent } from './admin-high-scores.component';
 
 describe('AdminHighScoresComponent', () => {
     let component: AdminHighScoresComponent;
     let fixture: ComponentFixture<AdminHighScoresComponent>;
+    let highScoresServiceSpy: jasmine.SpyObj<HighScoresService>;
 
     beforeEach(async () => {
+        highScoresServiceSpy = jasmine.createSpyObj('HighScoresService', ['getHighScores', 'openSnackBar', 'resetHighScores']);
+
         await TestBed.configureTestingModule({
             declarations: [AdminHighScoresComponent],
+            providers: [{ provide: HighScoresService, useValue: highScoresServiceSpy }],
         }).compileComponents();
     });
 
--- client/src/app/components/dialog-box-high-scores/dialog-box-high-scores.component.spec.ts
+++ client/src/app/components/dialog-box-high-scores/dialog-box-high-scores.component.spec.ts
@@ -1,75 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HighScoresService } from '@app/services/high-scores.service';
 import { DialogBoxHighScoresComponent } from './dialog-box-high-scores.component';
 
 describe('DialogBoxHighScoresComponent', () => {
     let component: DialogBoxHighScoresComponent;
     let fixture: ComponentFixture<DialogBoxHighScoresComponent>;
-    // let httpHandlerSpy: jasmine.SpyObj<HttpHandlerService>;
-    // const mockMatSnackBar = {
-    //     // eslint-disable-next-line @typescript-eslint/no-empty-function
-    //     open: () => {},
-    // };
+    let highScoresServiceSpy: jasmine.SpyObj<HighScoresService>;
 
-    // beforeEach(async () => {
-    //     httpHandlerSpy = jasmine.createSpyObj('HttpHandlerService', ['getClassicHighScore', 'getLOG2990HighScore']);
-    //     httpHandlerSpy.getClassicHighScore.and.returnValue(
-    //         of([{ _id: '932487fds', username: 'Vincent', type: 'Classique', score: 40, position: '1' }]),
-    //     );
-    //     httpHandlerSpy.getLOG2990HighScore.and.returnValue(
-    //         of([{ _id: '3256987sfdg', username: 'Vincent', type: 'LOG2990', score: 90, position: '4' }]),
-    //     );
-    //     await TestBed.configureTestingModule({
-    //         imports: [HttpClientModule, MatCardModule, MatIconModule],
-    //         declarations: [DialogBoxHighScoresComponent],
-    //         providers: [
-    //             { provide: HttpHandlerService, useValue: httpHandlerSpy },
-    //             { provide: MatSnackBar, useValue: mockMatSnackBar },
-    //         ],
-    //     }).compileComponents();
-    // });
+    beforeEach(async () => {
+        highScoresServiceSpy = jasmine.createSpyObj('HighScoresService', ['getHighScores', 'openSnackBar', 'resetHighScores']);
+        await TestBed.configureTestingModule({
+            declarations: [DialogBoxHighScoresComponent],
+            providers: [{ provide: HighScoresService, useValue: highScoresServiceSpy }],
+        }).compileComponents();
+    });
 
     beforeEach(() => {
         fixture = TestBed.createComponent(DialogBoxHighScoresComponent);
         component = fixture.componentInstance;
         fixture.detectChanges();
-        //  matSnackBar = TestBed.inject(MatSnackBar);
     });
 
     it('should create', () => {
         expect(component).toBeTruthy();
     });
-
-    // it('should call getClassicHighScore when calling getMessagesFromServer', () => {
-    //     component.getHighScores();
-    //     expect(httpHandlerSpy.getClassicHighScore).toHaveBeenCalled();
-    //     expect(httpHandlerSpy.getLOG2990HighScore).toHaveBeenCalled();
-    // });
-
-    // it('getHighScores should not call openSnackbar if we receive data from the server after 3 seconds', fakeAsync(() => {
-    //     const spy = spyOn(component, 'openSnackBar');
-    //     component.getHighScores();
-    //     tick(TIMEOUT);
-    //     expect(spy).not.toHaveBeenCalledWith(TEST_ERROR);
-    // }));
-
-    // it('getHighScores should  call openSnackbar if we do not receive data from the server after 3 seconds', fakeAsync(() => {
-    //     const spy = spyOn(component, 'openSnackBar');
-    //     component.getHighScores();
-    //     component.highScoreClassic = undefined;
-    //     component.highScoreLOG29990 = undefined;
-    //     tick(TIMEOUT);
-    //     expect(spy).toHaveBeenCalledWith(TEST_ERROR);
-    // }));
-
-    // it('openSnackBar should call the MatSnackBar open method', () => {
-    //     const matSnackBarSpy = spyOn(matSnackBar, 'open').and.stub();
-    //     component.openSnackBar(TEST_ERROR);
-    //     expect(matSnackBarSpy.calls.count()).toBe(1);
-    //     const args = matSnackBarSpy.calls.argsFor(0);
-    //     expect(args[0]).toBe(TEST_ERROR);
-    //     expect(args[1]).toBe('fermer');
-    //     expect(args[2]).toEqual({
-    //         verticalPosition: 'top',
-    //     });
-    // });
 });
