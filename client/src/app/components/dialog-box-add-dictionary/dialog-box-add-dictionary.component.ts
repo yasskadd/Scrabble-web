b6716fbb1753ae887064abe0478ba48ab104ae1c
Mariana Chang Ieong <mariana.8080@hotmail.com>
2022-04-06 01:25:19 -0400

    starting admin high scores

--- client/src/app/components/dialog-box-add-dictionary/dialog-box-add-dictionary.component.ts
+++ client/src/app/components/dialog-box-add-dictionary/dialog-box-add-dictionary.component.ts
@@ -1,41 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
-import { MatDialogRef } from '@angular/material/dialog';
-import { Observable } from 'rxjs';
-import { map, startWith } from 'rxjs/operators';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'app-dialog-box-add-dictionary',
     templateUrl: './dialog-box-add-dictionary.component.html',
     styleUrls: ['./dialog-box-add-dictionary.component.scss'],
 })
-export class DialogBoxAddDictionaryComponent implements OnInit {
-    myControl = new FormControl();
-    options: string[] = ['DictionaryOne', 'DictionaryTwo', 'DictionaryThree'];
-    filteredOptions: Observable<string[]>;
-
-    constructor(private dialogRef: MatDialogRef<DialogBoxAddDictionaryComponent>) {}
-
-    ngOnInit() {
-        this.filteredOptions = this.myControl.valueChanges.pipe(
-            startWith(''),
-            map((value) => this._filter(value)),
-        );
-    }
-
-    addDictionary() {
-        // TODO: add dictionary server side
-        // TODO: update dictionary list client side
-        this.closeDialog();
-    }
-
-    closeDialog() {
-        this.dialogRef.close();
-    }
-
-    private _filter(value: string): string[] {
-        const filterValue = value.toLowerCase();
-
-        return this.options.filter((option) => option.toLowerCase().includes(filterValue));
-    }
+export class DialogBoxAddDictionaryComponent {
+    // myControl = new FormControl();
+    // options: string[] = ['DictionaryOne', 'DictionaryTwo', 'DictionaryThree'];
+    // filteredOptions: Observable<string[]>;
+    // constructor(private dialogRef: MatDialogRef<DialogBoxAddDictionaryComponent>) {}
+    // ngOnInit() {
+    //     this.filteredOptions = this.myControl.valueChanges.pipe(
+    //         startWith(''),
+    //         map((value) => this._filter(value)),
+    //     );
+    // }
+    // addDictionary() {
+    //     // TODO: add dictionary server side
+    //     // TODO: update dictionary list client side
+    //     this.closeDialog();
+    // }
+    // closeDialog() {
+    //     this.dialogRef.close();
+    // }
+    // private _filter(value: string): string[] {
+    //     const filterValue = value.toLowerCase();
+    //     return this.options.filter((option) => option.toLowerCase().includes(filterValue));
+    // }
 }
