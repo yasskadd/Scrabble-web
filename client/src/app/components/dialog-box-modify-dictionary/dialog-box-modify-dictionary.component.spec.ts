d3ca53cc6aafb05375cd7495254f58f8a5f1f666
Vincent Richard <vincent.richard.01@gmail.com>
2022-04-05 21:38:08 -0400

    remove lint error

--- client/src/app/components/dialog-box-modify-dictionary/dialog-box-modify-dictionary.component.spec.ts
+++ client/src/app/components/dialog-box-modify-dictionary/dialog-box-modify-dictionary.component.spec.ts
@@ -1,25 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DialogBoxModifyDictionaryComponent } from './dialog-box-modify-dictionary.component';
 
 describe('DialogBoxModifyDictionaryComponent', () => {
-  let component: DialogBoxModifyDictionaryComponent;
-  let fixture: ComponentFixture<DialogBoxModifyDictionaryComponent>;
+    let component: DialogBoxModifyDictionaryComponent;
+    let fixture: ComponentFixture<DialogBoxModifyDictionaryComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DialogBoxModifyDictionaryComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DialogBoxModifyDictionaryComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DialogBoxModifyDictionaryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DialogBoxModifyDictionaryComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
