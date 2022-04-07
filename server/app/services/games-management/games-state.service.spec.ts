dcfcdfaea2410a9374d17d29a5494293330f347b
Vincent Richard <vincent.richard.01@gmail.com>
2022-04-07 15:47:41 -0400

    added test

--- server/app/services/games-management/games-state.service.spec.ts
+++ server/app/services/games-management/games-state.service.spec.ts
@@ -234,7 +234,7 @@ describe('GamesState Service', () => {
         expect(newPlayer).to.be.eql(EXPECTED_NEW_PLAYER as Player);
     });
 
-    it('createNewGame() should return a new game created', () => {
+    it('createNewGame() should return a new game created in classique mode', () => {
         sinon.stub(Turn);
         const TIMER = 60;
         const params = {
@@ -243,6 +243,23 @@ describe('GamesState Service', () => {
             timer: TIMER,
             socketId: [serverSocket.id],
             mode: 'classique',
+            botDifficulty: undefined,
+            dictionary: { words: ['string'] } as Dictionary,
+        };
+
+        gamesHandlerStub['gamePlayers'].set(player1.room, [player1, player2]);
+        const gameTest = gamesStateService['createNewGame'](params);
+        expect(gameTest !== undefined).to.eql(true);
+    });
+
+    it('createNewGame() should return a new game created in LOG2990 mode', () => {
+        const TIMER = 60;
+        const params = {
+            playerName: [player1.name, player2.name],
+            roomId: '1',
+            timer: TIMER,
+            socketId: [serverSocket.id],
+            mode: 'LOG2990',
             botDifficulty: undefined,
             dictionary: { words: ['string'] } as Dictionary,
         };
@@ -531,7 +548,17 @@ describe('GamesState Service', () => {
             player1.game = game;
             player2.game = game;
         });
-        it('endGameScore() should call deductPoints() of each player in a game if there is 6 consecutive skips', () => {
+        it('endGameScore() should call deductPoints() of each player in a game if there is 6 consecutive skips in the classic mode', () => {
+            player1.game.isMode2990 = false;
+            player1.game.turn.skipCounter = 6;
+            gamesHandlerStub['gamePlayers'].set(player1.room, [player1, player2]);
+
+            gamesStateService['endGameScore'](player1.room);
+            expect(player1.deductPoints.called).to.equal(true);
+        });
+
+        it('endGameScore() should call deductPoints() of each player in a game if there is 6 consecutive skips in the LOG2990 mode ', () => {
+            player1.game.isMode2990 = true;
             player1.game.turn.skipCounter = 6;
             gamesHandlerStub['gamePlayers'].set(player1.room, [player1, player2]);
 
@@ -543,6 +570,7 @@ describe('GamesState Service', () => {
                 'if his rack is empty and call deductPoints() for the other player',
             () => {
                 player1.rackIsEmpty.returns(true);
+                player1.game.isMode2990 = true;
                 player1.game.turn.skipCounter = 0;
                 gamesHandlerStub['gamePlayers'].set(player1.room, [player1, player2]);
 
@@ -679,6 +707,7 @@ describe('GamesState Service', () => {
         });
 
         it('initializePlayers() should call the setGame and the start function of the player when you are playing against a bot', (done) => {
+            botPlayer.game.isMode2990 = true;
             gamesStateService['initializePlayers']([realPlayer, botPlayer], botPlayer.game, [serverSocket.id]);
 
             expect(realPlayer.setGame.called).to.equal(true);
