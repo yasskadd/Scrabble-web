b404a40fa603da30aef4e17eb2ccce287c9c0046
ChristianSassine <71024958+ChristianSassine@users.noreply.github.com>
2022-04-14 23:32:50 -0400

    Finished dialog for game help tips :children_crossing:

--- client/src/app/components/dialog-game-help/dialog-game-help.component.spec.ts
+++ client/src/app/components/dialog-game-help/dialog-game-help.component.spec.ts
@@ -1,25 +1,23 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { DialogGameHelpComponent } from './dialog-game-help.component';
 
 describe('DialogGameHelpComponent', () => {
-  let component: DialogGameHelpComponent;
-  let fixture: ComponentFixture<DialogGameHelpComponent>;
+    let component: DialogGameHelpComponent;
+    let fixture: ComponentFixture<DialogGameHelpComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ DialogGameHelpComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [DialogGameHelpComponent],
+        }).compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(DialogGameHelpComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(DialogGameHelpComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
