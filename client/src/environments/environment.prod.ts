a5922e7f47d59a3f97be3df1df238015858ae274
yasserkadimi <yasser.kadimi@gmail.com>
2023-06-20 21:24:32 -0400

    change ec2-instance url

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    serverUrl: 'http://ec2-35-183-206-23.ca-central-1.compute.amazonaws.com:3000',
+    serverUrl: 'http://ec2-3-88-21-251.compute-1.amazonaws.com:3000',
 };
